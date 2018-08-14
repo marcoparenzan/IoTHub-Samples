a5bac4212dd4e03eb2958201611e800510360dec
Marco Parenzan <marco.parenzan@live.it>
2018-08-14 02:15:36 +0200

    Too many files and SAS

--- VerneMQ MQTT Broker/monitor.bat
+++ VerneMQ MQTT Broker/monitor.bat
@@ -1,1 +1,1 @@
-iothub-explorer monitor-events dev1 --login HostName=vernemq.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=rjpPUJ+xJUMYox66L6w45hBYdzkESdeFRV7FE4tqtBk=
+iothub-explorer monitor-events dev1 --login 
